0f65a187d9bdaf2367b43269602ecda264827d6f
Tuong Truong <ttruong2621@sdsu.edu>
2026-03-19 20:47:54 -0700

    updating with when a semester being clicked it also close the menu as well

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 // ALL YOU JS GOES HERE
 
+//Menu button to open and close the semester selector
 const menuButton = document.querySelector('.menu_button').classList;
 const semester = document.querySelector('.semester_list').classList;
-  
-
 
 function toggleSemester() {
     semester.toggle('semester_vertical');
@@ -15,3 +14,18 @@ function toggleMenu() {
     console.log('menubutton clicked');
     toggleSemester();
 }
+
+//Event listener to close the semester selector when clicking
+const semesterList = document.querySelectorAll('.semesterlink');
+
+function closeSemester() {
+    semester.remove('semester_vertical');
+    menuButton.remove('change');
+    console.log('semester closed');
+}
+
+//loop through each semester link and add event listener to close the semester selector when clicking
+
+semesterList.forEach(link => {
+    link.addEventListener('click', closeSemester);
+});
