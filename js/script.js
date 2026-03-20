341f6d0e8c782ea0dbddfd4508a3899bc86fed4a
Tuong Truong <ttruong2621@sdsu.edu>
2026-03-19 20:02:23 -0700

    Creating a function to display the semester lists once it activate

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // ALL YOU JS GOES HERE
+
+const menuButton = document.querySelector('.menu_button').classList;
+const semester = document.querySelector('.semester_list').classList;
+  
+
+
+function toggleSemester() {
+    semester.toggle('semester_vertical');
+    console.log('semester show up');
+}
+
+function toggleMenu() {
+    menuButton.toggle('change'); 
+    console.log('menubutton clicked');
+    toggleSemester();
+}
