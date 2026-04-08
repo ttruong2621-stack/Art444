c9daa7a3c077ff287bbc05ec8e5690e0339d96cd
Tuong Truong <ttruong2621@sdsu.edu>
2026-04-07 20:34:57 -0700

    Adding various color function to change

--- js/script.js
+++ js/script.js
@@ -29,3 +29,41 @@ function closeSemester() {
 semesterList.forEach(link => {
     link.addEventListener('click', closeSemester);
 });
+
+
+//Color setting function
+//primary color changer
+const setPrimaryColor = (color) => {
+    document.documentElement.style.setProperty('--primary-color', color);
+};
+
+//secondary color changer
+const setSecondaryColor = (color) => {
+    document.documentElement.style.setProperty('--secondary-color', color);
+};
+
+//Tertiary color changer
+const setTertiaryColor = (color) => {
+    document.documentElement.style.setProperty('--tertiary-color', color);
+};
+
+//Section color changer
+const setSectionColor = (color) => {
+    document.documentElement.style.setProperty('--section-color', color);
+};
+
+//Primary text color changer
+const setPrimaryTextColor = (color) => {
+    document.documentElement.style.setProperty('--primary-text-color', color);
+};
+
+
+//Light and dark mode toggle
+function lightDarkMode() {
+    setPrimaryColor();
+    setSecondaryColor();
+    setTertiaryColor();
+    setSectionColor();
+    setPrimaryTextColor();
+    console.log('dark background set');
+}
